f739dd83d6bb653fceb8de7f82e12d5a794dd704
张许妍 <181250187@smail.nju.edu.cn>
2022-05-26 22:54:35 +0800

    feat : api

--- src/api/http-request.js
+++ src/api/http-request.js
@@ -69,24 +69,62 @@ export const get = function (path, data, success, error, final) {
  * @param error
  * @param final
  */
-// export const delete = function (path, data, success, error, final) {
-//     let result = axios
-//         .post(path, data, Config.config)
-//         .then(res => {
-//             if (res.status.toString().startsWith('2')) {
-//                 success(res.data)
-//             } else {
-//                 error(res?.statusText)
-//                 throw res?.statusText
-//             }
-//         })
-//         .catch(err => {
-//             error(err?.message)
-//             throw err?.message
-//         })
-//         .finally(() => {
-//             if (typeof final === 'function') final()
-//         })
-//
-//     return result
-// }
+export const delete1 = function (path, success, error, final) {
+    let result = axios
+        .delete(path, Config.config)
+        .then(res => {
+            if (res.status.toString().startsWith('2')) {
+                success(res.data)
+            } else {
+                error(res?.statusText)
+                throw res?.statusText
+            }
+        })
+        .catch(err => {
+            error(err?.message)
+            throw err?.message
+        })
+        .finally(() => {
+            if (typeof final === 'function') final()
+        })
+
+    return result
+}
+
+/**
+ * 封装put请求
+ * @param path
+ * @param data
+ * @param success
+ * @param error
+ * @param final
+ */
+export const put = function (path, data, success, error, final) {
+    let config = {
+        body: data,
+        responseType: 'text',
+        headers: {
+            'Content-type': 'application/json',
+            Authorization: Config.config.headers.Authorization
+        }
+    }
+    let result = axios
+        .put(path, data,config)
+        .then(res => {
+            if (res.status.toString().startsWith('2')) {
+                success(res.data)
+            } else {
+                error(res?.statusText)
+                throw res?.statusText
+            }
+        })
+        .catch(err => {
+            error(err?.message)
+            throw err?.message
+        })
+        .finally(() => {
+            if (typeof final === 'function') final()
+        })
+
+    return result
+}
--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,249 @@
 import * as Config from '@/config'
-import { post, get } from '@/api/http-request' // eslint-disable-line no-unused-vars
+import {post, get, delete1, put} from '@/api/http-request' // eslint-disable-line no-unused-vars
 
 const endpoint = '/rules-engine';
 const version = '';
 const urlPrefixV2 = `${Config.HTTP_SERVER}${endpoint}${version}`;
 const pingUrl = `${urlPrefixV2}/ping`;
 const streamUrl = `${urlPrefixV2}/streams`;
+const ruleUrl = `${urlPrefixV2}/rules`;
 
 export function allStreams(success, error, final) {
-    return get(streamUrl,null, success, error, final)
+    return get(streamUrl, null, success, error, final)
 }
 
 export function ping(success, error, final) {
     return get(pingUrl, null, success, error, final)
 }
+
+export function addStream(streamSql, success, error, final) {
+    return post(
+        `${streamUrl}`,
+        streamSql,
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('addStream error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function deleteOneStreamById(id, success, error, final) {
+    let url = `${streamUrl}/${id}`;
+    return delete1(
+        url,
+        res => {
+            res = res[0];
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message);
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('deleteOneStreamById error:', err);
+            error(err);
+            throw err
+        },
+        final
+    )
+}
+
+export function updateStream(sqlformatOfStreamData, streamNameOrID, success, error, final) {
+    let url = `${streamUrl}/${streamNameOrID}`;
+    return put(
+        url,
+        sqlformatOfStreamData,
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('updateStream error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function findStreamByName(name, success, error, final) {
+    let url = `${streamUrl}/${name}`;
+    return get(url, null, success, error, final)
+}
+
+export function addRule(rule, success, error, final) {
+    return post(
+        `${ruleUrl}`,
+        rule,
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('addStream error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function deleteOneRuleById(id, success, error, final) {
+    let url = `${ruleUrl}/${id}`;
+    return delete1(
+        url,
+        res => {
+            res = res[0];
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message);
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('deleteOneStreamById error:', err);
+            error(err);
+            throw err
+        },
+        final
+    )
+}
+
+export function allRules(success, error, final) {
+    return get(ruleUrl, null, success, error, final)
+}
+
+export function getRuleStatusMetricsById(id, success, error, final) {
+    let url = `${ruleUrl}/${id}/status`;
+    return get(url, null, success, error, final)
+}
+
+export function startRule(id, success, error, final) {
+    let url = `${ruleUrl}/${id}/start`;
+    return post(
+        url,
+        null,
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('startRule error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function restartRule(id, success, error, final) {
+    let url = `${ruleUrl}/${id}/restart`;
+    return post(
+        url,
+        null,
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('restartRule error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function stopRule(id, success, error, final) {
+    let url = `${ruleUrl}/${id}/stop`;
+    return post(
+        url,
+        null,
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('stopRule error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function findRuleById(id, success, error, final) {
+    let url = `${ruleUrl}/${id}`;
+    return get(url, null, success, error, final)
+}
+
+export function updateRule(rule, success, error, final) {
+    let url = `${ruleUrl}/${rule.id}`;
+    return put(
+        url,
+        JSON.stringify(rule),
+        res => {
+            res = res[0]
+            if (res.statusCode.toString().startsWith('2')) {
+                success(res)
+            } else {
+                error(res?.message)
+                throw res?.message
+            }
+        },
+        err => {
+            console.log('updateRule error:', err)
+            error(err)
+            throw err
+        },
+        final
+    )
+}
+
+export function getRuleTopoById(id, success, error, final) {
+    let url = `${ruleUrl}/${id}/topo`;
+    return get(url, null, success, error, final)
+}
+
+
+
 
 
